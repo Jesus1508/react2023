83c264a96a40670285efcb0354a735113a9d8b57
Jesus Solis Martinez <iscjesus1508@gmail.com>
2023-11-13 16:55:54 -0600

    Iniciando con React

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,15 @@
+export const FirstApp = () => {
+
+    const newMessage = {
+        message: 'Hola Mundo',
+        title: 'Jesus'
+    }
+
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            {/* <code>{ JSON.stringify(newMessage) }</code> */}
+            <p>soy un subtitulo</p>
+        </>
+    )
+}
