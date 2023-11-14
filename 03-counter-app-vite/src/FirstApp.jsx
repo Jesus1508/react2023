1d17b0abf050c6ef38e47a9fbfbd97e8e8e8c7c0
Jesus Solis Martinez <iscjesus1508@gmail.com>
2023-11-14 16:47:03 -0600

    Continuando con ejercicio de React

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,15 +1,23 @@
-export const FirstApp = () => {
+import PropTypes from 'prop-types'
 
-    const newMessage = {
-        message: 'Hola Mundo',
-        title: 'Jesus'
-    }
-
+export const FirstApp = ( { title, subtitle, name } ) => {
+    
     return (
         <>
-            <h1>Hola Mundo</h1>
-            {/* <code>{ JSON.stringify(newMessage) }</code> */}
-            <p>soy un subtitulo</p>
+            <h1>{ title }</h1>
+            <p>{ subtitle }</p>
+            <p>{ name }</p>
         </>
     )
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+}
+
+FirstApp.defaultProps = {
+    name: 'Jesus Solis Martinez',
+    subtitle: 'No hay subtitulo',
+    title: 'No hay nigun titulo',
 }
