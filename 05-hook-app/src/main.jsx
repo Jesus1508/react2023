cc62484f34aadce370ba19304469a2b32b48c7db
Jesus Solis Martinez <iscjesus1508@gmail.com>
2023-11-22 22:02:17 -0600

    Fix: "hooks" useMemo, useCallback de React cambiando ubicación de archivos

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -11,7 +11,7 @@ import ReactDOM from 'react-dom/client'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { Memorize } from './06-memos/Memorize'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-import { Padre } from './07-tarea-memo/07-tarea-memo/Padre'
+import { Padre } from './07-tarea-memo/Padre'
 // import { SimpleForm  from './02-useEffect/SimpleForm'
 
 import './index.css'
