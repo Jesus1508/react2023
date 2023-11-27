472b3b81c18d7df87e15f9cd95d0519ae0d22950
Jesus Solis Martinez <iscjesus1508@gmail.com>
2023-11-27 16:58:02 -0600

    Iniciando con HeroesApp en React

---
+++ 07-heroes-spa/src/HeroesApp.jsx
@@ -0,0 +1,11 @@
+import { AppRouter } from "./router/App.Router"
+
+const HeroesApp = () => {
+    return (
+        <>
+            <AppRouter />
+        </>
+    )
+}
+
+export default HeroesApp
---
+++ 07-heroes-spa/src/main.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import HeroesApp from './HeroesApp'
+
+import './styles.css'
+
+ReactDom.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <HeroesApp/>
+        </BrowserRouter>
+    </React.StrictMode>
+)
