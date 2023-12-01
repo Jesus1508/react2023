0c03d54adf9733c2983aa56c046736fbf8089d27
Jesus Solis Martinez <iscjesus1508@gmail.com>
2023-12-01 15:04:15 -0600

    Iniciando con protección de rutas en React

--- 07-heroes-spa/src/HeroesApp.jsx
+++ 07-heroes-spa/src/HeroesApp.jsx
@@ -1,10 +1,13 @@
+import { AuthProvider } from "./auth"
 import { AppRouter } from "./router/App.Router"
 
 const HeroesApp = () => {
     return (
-        <>
+        <AuthProvider>
+
             <AppRouter />
-        </>
+            
+        </AuthProvider>
     )
 }
 
