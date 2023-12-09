1fcf379a7b9b297a206b95ba74da7355854e4a43
Jesus Solis Martinez <iscjesus1508@gmail.com>
2023-12-08 19:42:52 -0600

    Terminado diseño de JournalApp

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,13 +1,29 @@
+import { IconButton } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
-import { NothingSelectedView } from '../views'
+import { NoteView, NothingSelectedView } from '../views'
+import { AddOutlined } from '@mui/icons-material'
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
             
             <NothingSelectedView />
+
+            <IconButton 
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }}/>
+            </IconButton>
             
-            {/* NoteView */}
+            {/* <NoteView /> */}
         </JournalLayout>
     )
 }
