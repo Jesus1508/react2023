802cd9e01bc9d98b9e74ffd2575abd568c70ce82
Jesus Solis Martinez <iscjesus1508@gmail.com>
2023-12-24 17:54:02 -0600

    Finalizando con la autenticación de firebase

--- 08-journal-app/src/journal/components/SideBar.jsx
+++ 08-journal-app/src/journal/components/SideBar.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/prop-types */
 import { TurnedInNot } from '@mui/icons-material'
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
+import { useSelector } from 'react-redux'
 
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio']
 
 export const SideBar = ({ drawerWidth = 240 }) => {
+
+    const { displayName } = useSelector( state => state.auth )
+
     return (
         <Box
             component='nav'
@@ -20,7 +24,7 @@ export const SideBar = ({ drawerWidth = 240 }) => {
             >
                 <Toolbar>
                     <Typography variant='h6' noWrap component='div'>
-                        Jesus Solis
+                        { displayName }
                     </Typography>
                 </Toolbar>
                 <Divider/>
